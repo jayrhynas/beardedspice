760923c6535a256fc76154884dba2d20ebc57d65
Jayson Rhynas <jayrhynas@gmail.com>
2014-10-25 15:13:15 -0400

    Initial commit of Today extension template

---
+++ BeardedSpiceToday/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  BeardedSpiceToday
+//
+//  Created by Jayson Rhynas on 2014-10-25.
+//  Copyright (c) 2014 Tyler Rhodes / Jose Falcon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TodayViewController : NSViewController
+
+@end
